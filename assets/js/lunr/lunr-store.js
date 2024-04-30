66a16da8cc07c6aebec38b95b8e983dbc859fc60
clegoues <clegoues@cs.cmu.edu>
2024-04-30 03:55:09 +0000

    Deploying to master from @ clegoues/clegoues.github.io@dc0f3335098e8cb7aaa8b3af0874afd8a9be5403 🚀

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,73 @@
+var store = [{
+        "title": "Double-blind review at SSBSE",
+        "excerpt":"As PC co-chair for the Symposium for Search-Based Software Engineering in 2014, I participated in the Organizing Committee’s discussion about and decision to implement double-blind submission review for the main research track. On behalf of the Organizing Committee, I composed a post explaining the reasoning behind the experiment. That post...","categories": [],
+        "tags": ["academia","anonymousreview"],
+        "url": "/posts/ssbse-anon-review.html",
+        "teaser": null
+      },{
+        "title": "Industry vs. Academia, or, the Grey Lady Misses the Point",
+        "excerpt":"Emery peer pressured me to blog, presumably rather than ranting entirely in Facebook posts, which is where a moderately shorter version of this first appeared. I admit this does feel a hair more legitimate. At least, I feel less compelled to apologize for the length.) This article: Uber Would Like to...","categories": [],
+        "tags": ["academia","rant","science","technology","yourtaxdollarsatwork"],
+        "url": "/posts/2015/09/industry-academia",
+        "teaser": null
+      },{
+        "title": "One Motto, Three Rules of Thumb",
+        "excerpt":"SSSG[1], the ISR SE program’s weekly research symposium, typically features the SE PhD students (and, less commonly, faculty) presenting either their work, or surveys of the work of others.  From time to time, though, we go a bit meta. This week, I gave a talk on talks, specifically on how I approach the task of structuring...","categories": [],
+        "tags": ["academia","talks"],
+        "url": "/posts/2015/10/talk-structure",
+        "teaser": null
+      },{
+        "title": "CS PhDs and US Immigration Policy, a Long and Pointlessly Insane Saga",
+        "excerpt":"(Should I be working on a grant proposal? Yes, yes I should. Am I writing a blog post instead? Yes, yes I am.) I’m beginning to wonder if anyone at the NYT actually knows anything about higher education/research in STEM. Exhibit N: an ENTIRE ARTICLE about whether STEM graduates should...","categories": [],
+        "tags": ["academia","grantprocrastination","immigration","policy","rant","yourtaxdollarsatwork"],
+        "url": "/posts/2015/11/us-immigration-phds",
+        "teaser": null
+      },{
+        "title": "Free/Fair? Or, A Somewhat Bizarre Request to Fellow Harvard Alums",
+        "excerpt":"Hey, fellow Harvard alums: This year, when you get a ballot for the Harvard Board of Overseers Election in the snail-mail, instead of throwing it away without looking at it: don’t. Instead: vote. The Board of Overseers is a group of 30 individuals each serving 8-year terms. Harvard says: Drawing on the&nbsp;wide-ranging experience...","categories": [],
+        "tags": ["academia","atleasttheyreelected","boardofoverseers","diversity","harvard","meritocracy","rant","scarequotes"],
+        "url": "/posts/2016/01/free-fair-harvard",
+        "teaser": null
+      },{
+        "title": "What The Bachelor Teaches Us About Choosing a PhD Advisor",
+        "excerpt":"Two of my  ongoing professional quests are to provide insight the processes of CS academia to those who would benefit from it and to increase the number of people who meet me and say “Oh, I know about you!  Jean Yang mentioned you on her blog!”  To those ends, over at...","categories": [],
+        "tags": ["academia","advising","jeanyang"],
+        "url": "/posts/2016/03/bachelor-advisor",
+        "teaser": null
+      },{
+        "title": "A reluctant ICSE submission cap post or, an exploration of primary sources",
+        "excerpt":"(Context: I was on the ICSE 2016 PC and I am on the ICSE 2017 PC.  I have never submitted more than three papers to ICSE.) Much recent brouhaha in the software engineering research community on the new 3-submissions-per-any-individual-author cap imposed by the ICSE 2017 organizing committee.  I’ve been resisting wading into this,...","categories": [],
+        "tags": ["academia","icse","pc","reluctantrant"],
+        "url": "/posts/2016/04/icse-submission-cap1",
+        "teaser": null
+      },{
+        "title": "A reluctant ICSE submission cap post or, an exploration of primary sources",
+        "excerpt":"[ETA: the Townhall will start at 5:45 in the Glass Oaks room, and will include time for discussion and questions, as well as a panel of experts and senior members of the community who will address the issue.] I’ve been corresponding with Tom Zimmermann of Microsoft Research, who has analyzed ICSE...","categories": [],
+        "tags": ["academia","icse","pc"],
+        "url": "/posts/2016/05/icse-submission-cap2",
+        "teaser": null
+      },{
+        "title": "Things I Keep Repeating About Writing",
+        "excerpt":"I often write papers with students, or read students’ papers to provide comments, and I find myself saying the same things over and over, especially the first time out.*  So: here’s a blog post I can point them to to (hopefully!) save us all some time and trouble. I plan to update it as I...","categories": [],
+        "tags": ["academia","latex","students","writing"],
+        "url": "/posts/clg-writing-rules.html",
+        "teaser": null
+      },{
+        "title": "My original PWLConf reading list on patch generation",
+        "excerpt":"I loved speaking at the inaugural Papers We Love conference, co-located with Strange Loop, in the disarmingly cool city of St. Louis. I’d never been to or spoken at a PWL event before, but I’ve had a great time getting to know the community. The basic idea is a bunch...","categories": [],
+        "tags": ["programrepair","pwlconf"],
+        "url": "/posts/2016/09/pwlconf-reading-list",
+        "teaser": null
+      },{
+        "title": "Conference Networking, or, Just Talk to Whomever, with Anecdotes",
+        "excerpt":"It’s summer conference time, and Phil Guo recently posted a handy how-to on “Attending Professional Conferences as a Newcomer”, which reminded me to finish this one up. His post is probably more actionable; here, I elaborate on a related piece of advice in a way that is hopefully complementary.  tl;dr: My...","categories": [],
+        "tags": ["academia","students","conferences"],
+        "url": "/posts/2017/05/conference-networking",
+        "teaser": null
+      },{
+        "title": "GenProg GitHub Launch",
+        "excerpt":"My collaborators and I started GenProg, “Genetic/Generic Program Repair (Depending on Whom You Ask)”, in 2008, maybe a month or so after GitHub was launched.  My grad school research group was hip and up-to-date in our development tools, primarily evidenced by the fact that we used Subversion instead of CVS....","categories": [],
+        "tags": ["programrepair"],
+        "url": "/posts/2017/09/genprog-github-launch",
+        "teaser": null
+      }]
